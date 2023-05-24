6b24a3d6754c49ec294a011608c24175efede29b
Alan-95-hub <aibishaev@edu.hse.ru>
2023-05-24 23:53:23 +0300

    s

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(123)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {"key": "Hello"}
+
+@app.get('/{pk}')
+def get_item(pk:int):
+    return {"key": pk}
